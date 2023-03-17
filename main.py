03f8e0281c746910606d1ff00dad849692a32566
gviclin <gviclin@gmail.com>
2023-03-17 22:44:17 +0100

    Add

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import xlrd
-import pandas
+import pandas as pd
 import sys
+from datetime import datetime
 
-def echo(phrase: str) -> None:
-   """A dummy wrapper around print."""
-   # for demonstration purposes, you can imagine that there is some
-   # valuable and reusable logic inside this function
-   print(phrase)
-   print("annotation : "+ echo.__annotations__['phrase'])
 
 def main() -> int:
-    df = pandas.read_excel("/home/gv/Téléchargements/export_17_03_2023_20_00_12.xls")
-    echo("Columns")
-    echo(df.columns)
+    myBook = xlrd.open_workbook("/home/gv/Téléchargements/export_17_03_2023_20_00_12.xls")
+
+    mySheet = myBook.sheet_by_index(0)
+
+    da = mySheet.cell_value(5, 0)
+    print("date :", da)
+
+    d = datetime.strptime(da, '%d-%m-%Y')
+    print ("type : ",type(d))
+    print("date :", d)
+    # df = pd.read_excel
+    # print("dataframe :")
+    # print(df.columns.values.tolist())
+
+    # print(df.head(10))
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())  # next section explains the use of sys.exit
-
